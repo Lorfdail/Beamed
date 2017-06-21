134f52b321cb8cd9470d10133fbdf28d9d3ef1ac
Lorfdail <michael.niens@googlemail.com>
2017-06-21 22:47:09 +0200

    Lets Rest a bit~
    - Started implementing the endpoints now
    - the mixerclient and mixerrestclient now implement the IDisposable interface

--- src/Beamed.Core/MixerClient.cs
+++ src/Beamed.Core/MixerClient.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Beamed.Core {
-  public class MixerClient {
+  public class MixerClient : IDisposable {
     public bool Authenticated { get; private set; } = false;
     public string Token { get; private set; }
+
+    public void Dispose()
+    { 
+        Dispose(true);
+        GC.SuppressFinalize(this);           
+    }
+
+    protected virtual void Dispose(bool disposing) { }
   }
 }
